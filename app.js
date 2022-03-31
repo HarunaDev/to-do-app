1816d514dd7ee531a3a1986e0debc04d39e407cc
Haruna Alvin <harunaalvin69@gmail.com>
2022-03-31 01:13:52 +0100

    correctedtypo with ol-el in js

--- app.js
+++ app.js
@@ -2,5 +2,7 @@
 let myList = []
 const inputEl = document.getElementById("input-el")
 const saveBtn = document.getElementById("save-btn")
-const ulEl = document.getElementById("ul-el")
+const olEl = document.getElementById("ol-el")
 const talkBtn = document.querySelector(".talk")
+
+
