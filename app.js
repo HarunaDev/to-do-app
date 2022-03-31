8c2fda743f837bc734707d255da258ea4c13ca0b
Haruna Alvin <harunaalvin69@gmail.com>
2022-03-31 01:18:07 +0100

    initialised speech api

--- app.js
+++ app.js
@@ -5,4 +5,19 @@ const saveBtn = document.getElementById("save-btn")
 const olEl = document.getElementById("ol-el")
 const talkBtn = document.querySelector(".talk")
 
+// initialise speech recognition
 
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition; 
+
+const recognition = new SpeechRecognition()
+
+// on start
+recognition.onstart = () => {
+    console.log("Voice Activated");
+}
+
+// on result
+recognition.onresult = (e) => {
+    console.log("done");
+
+}
