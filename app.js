dc0bd8bc8fb46b44a8f6bace40479f1bc8384595
Haruna Alvin <harunaalvin69@gmail.com>
2022-03-31 01:10:57 +0100

    grabbed elements in js file

---
+++ app.js
@@ -0,0 +1,6 @@
+// grab elements
+let myList = []
+const inputEl = document.getElementById("input-el")
+const saveBtn = document.getElementById("save-btn")
+const ulEl = document.getElementById("ul-el")
+const talkBtn = document.querySelector(".talk")
